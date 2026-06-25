34b70a47816feacc10ed4d8cf0bd019ca0b57d12
umiedev <umierohaizad@gmail.com>
2026-06-25 22:25:08 +0800

    push

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        //testing push ni kalau dah edit push so senang org lain nk pull dari github collab
     }
 }
+//save dulu
